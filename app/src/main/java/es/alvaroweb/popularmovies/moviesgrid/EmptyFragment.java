0723a8c1cf13bdefcac9a421bcf26e161514fb94
alvaro893 <alvarobolro@gmail.com>
2016-06-29 18:19:06 +0300

    feat: add an empty fragment and remove ui bugs

---
+++ app/src/main/java/es/alvaroweb/popularmovies/moviesgrid/EmptyFragment.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2016 Alvaro Bolanos Rodriguez
+ */
+package es.alvaroweb.popularmovies.moviesgrid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.alvaroweb.popularmovies.R;
+
+/*
+ * TODO: Create JavaDoc
+ */
+public class EmptyFragment extends Fragment {
+
+    public EmptyFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_empty, container, false);
+
+    }
+}
